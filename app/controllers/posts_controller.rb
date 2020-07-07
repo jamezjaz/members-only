c42f1962409bb31affa9080260a68c05049b068c
Jamezjaz <jamezjaz@gmail.com>
2020-07-07 14:04:08 -0500

    Adds user authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,26 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!, only [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create]
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+        @post_user = current_user.id
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:body, :user_id)
+    end
 end
