7e1638aa0add0063c67ddf3d6ee8e19ca7f4737d
Jamezjaz <jamezjaz@gmail.com>
2020-07-07 12:54:08 -0500

    Adds user_id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,3 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, only [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_173705) do
+ActiveRecord::Schema.define(version: 2020_07_07_174918) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
